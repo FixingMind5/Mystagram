8f1de9f54b1a886f36e81ff01045e3460b645159
FixingMind5 <aguilarmanu5@gmail.com>
2019-01-09 11:04:21 -0600

    Improving administrator dashboard

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,8 @@
 """User admin classes"""
 
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 from users.models import Profile
 
 @admin.register(Profile)
@@ -24,3 +26,47 @@ class ProfileAdmin(admin.ModelAdmin):
                 'user__is_active',
                 'user__is_staff'
             )
+
+
+    fieldsets = (
+        ('Profile', {
+            'fields': (
+                 ('user', 'picture'),),
+        }),
+        ('Extra info', {
+            'fields': (
+                ('website', 'phone_number'),
+                ('biography')
+            )
+        }),
+        ('Metadata', {
+            'fields': (('modified'),),
+        })
+    )
+
+    readonly_fields = ('created', 'modified')
+
+
+class ProfileInLine(admin.StackedInline):
+    """Profile in-line admin for users"""
+
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profiles'
+
+
+class UserAdmin(BaseUserAdmin):
+    """Add profile admin to base user admin"""
+
+    inlines = (ProfileInLine,)
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_active',
+        'is_staff'
+    )
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
