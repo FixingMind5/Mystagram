59d5295cc9e84f07c2314c0481a64d8d066d0acf
FixingMind5 <aguilarmanu5@gmail.com>
2019-01-08 22:40:37 -0600

    Borrar todo :C

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -1,12 +1,14 @@
 """URL'S modules of platzigram"""
 from django.urls import path
+from django.contrib import admin
 from platzigram import views
 from posts import views as posts_views
 
 
 urlpatterns = [
-    path('hello', views.hola_mundo),
-    path('sorted', views.sorted_integers),
-    path('hi/<str:name>/<int:age>', views.say_hi),
+    path('admin/', admin.site.urls),
+    path('hello/', views.hola_mundo),
+    path('sorted/', views.sorted_integers),
+    path('hi/<str:name>/<int:age>/', views.say_hi),
     path('posts/', posts_views.lists_posts),
 ]
